aeea0427324772ec0091001a618801ca4d2dff8d
Aashey <dazewaker12345@gmail.com>
2024-05-25 12:18:53 +0545

    Conversion data types

--- 01_basics/datatypeconversion.js
+++ 01_basics/datatypeconversion.js
@@ -1,7 +1,32 @@
 // let score = '33abc';
 let a = null;
+let score = [null,undefined,"31abc",31,"Aashish",true];
+let result = []
+for(let i = 0; i< score.length;i++){
+    let valueInNumber = Number(score[i]);
+    console.log(score[i],typeof valueInNumber); // js converts everything into number
+    result.push(valueInNumber); //but what do we get if we display those numbers ?
+}
 
-console.log(typeof score);
+console.table(result); // we get this
 
-let valueInNumber = Number(score); // number but how ?
-console.log(valueInNumber); // NAN
+// Notes
+/*
+"31" => 31
+"31abc" => NaN
+true => 1; false => 0
+*/
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0 => false
+"" => false; "asmd" => true
+*/
+
+let someNum = 1;
+let stringNum = String(someNum);
+console.log(stringNum);
+console.log(typeof stringNum);

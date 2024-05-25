a5100e2f91c09ed9708dd13daee8012e7bc35597
Aashey <dazewaker12345@gmail.com>
2024-05-25 13:28:24 +0545

    Addon to the file

--- 01_basics/datatypeconversion.js
+++ 01_basics/datatypeconversion.js
@@ -4,11 +4,11 @@ let score = [null,undefined,"31abc",31,"Aashish",true];
 let result = []
 for(let i = 0; i< score.length;i++){
     let valueInNumber = Number(score[i]);
-    console.log(score[i],typeof valueInNumber); // js converts everything into number
+    // console.log(score[i],typeof valueInNumber); // js converts everything into number
     result.push(valueInNumber); //but what do we get if we display those numbers ?
 }
 
-console.table(result); // we get this
+// console.table(result); // we get this
 
 // Notes
 /*
@@ -19,7 +19,7 @@ true => 1; false => 0
 
 let isLoggedIn = "";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /*
 1 => true; 0 => false
@@ -28,5 +28,39 @@ console.log(booleanIsLoggedIn);
 
 let someNum = 1;
 let stringNum = String(someNum);
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+//-------------------operations-----------------------------
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " james"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log(("1" + 2) + 2);
+// console.log((1 + 2) + "2");
+// console.log(+true); // 1
+// console.log(+false); // 0
+// console.log(+""); // 0
+let gameCounter = 100
+gameCounter++ ;
+// console.log(gameCounter);
+
+let x = 2;
+const y = x++; // in this y stores the value before increment of x
+console.log(x,y);
+
+let p = 2;
+const q = ++p;// in this q increments and then stores the value of p
+console.log(p,q);

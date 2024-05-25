42bb8e5535abbfdf90f82e28ae0c14ff0016181e
Aashey <dazewaker12345@gmail.com>
2024-05-25 19:40:12 +0545

    UPdated array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,5 +1,34 @@
-const name = "Google";
+// Array
 
-const surname = "Bituwa"
+const myArr = [0,1,2,3,4,5]
+//or
+const fruits = new Array("Apple","Mango","Banana");
 
-console.log(`${name} ${surname}`)
+// console.log(fruits);
+
+// Array Methods
+
+myArr.push(6,7,8);
+myArr.pop(); // remove last element
+myArr.unshift(9) // insert 9 in the first index
+myArr.shift() // remove 9 from the first index
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(4));
+// const newArr = myArr.join();
+// console.log(newArr);
+// console.log(typeof newArr);
+// console.log(myArr);
+// console.log(typeof myArr);
+
+//slice, splice
+// console.log("A: ",myArr);
+
+// const myn1 = myArr.slice(1,3);
+// console.log(myn1);
+// console.log("B: ", myArr);
+
+// const myn2 = myArr.splice(1,3); // extracts the elements out of the array
+// console.log(myn2); // elements other than spliced elements remain in the array
+// console.log("C: ",myArr);
+
+

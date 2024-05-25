1c1fd0f87975c6603a3653d7bbe7fed179df3c4c
Aashey <dazewaker12345@gmail.com>
2024-05-25 20:43:33 +0545

    updated

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,17 +1,18 @@
 // Array
 
-const myArr = [0,1,2,3,4,5]
+const myArr = [0, 1, 2, 3, 4, 5];
 //or
-const fruits = new Array("Apple","Mango","Banana");
+const fruits = new Array("Apple", "Mango", "Banana");
+const ap = 22;
 
 // console.log(fruits);
 
 // Array Methods
 
-myArr.push(6,7,8);
+myArr.push(6, 7, 8);
 myArr.pop(); // remove last element
-myArr.unshift(9) // insert 9 in the first index
-myArr.shift() // remove 9 from the first index
+myArr.unshift(9); // insert 9 in the first index
+myArr.shift(); // remove 9 from the first index
 // console.log(myArr.includes(5));
 // console.log(myArr.indexOf(4));
 // const newArr = myArr.join();
@@ -30,5 +31,3 @@ myArr.shift() // remove 9 from the first index
 // const myn2 = myArr.splice(1,3); // extracts the elements out of the array
 // console.log(myn2); // elements other than spliced elements remain in the array
 // console.log("C: ",myArr);
-
-

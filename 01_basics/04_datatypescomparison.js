9ab55b26253c0eab8eded4470fc464ad6f6c6551
Aashey <dazewaker12345@gmail.com>
2024-05-25 13:40:33 +0545

    Adding a new file datatypecomaprison

---
+++ 01_basics/04_datatypescomparison.js
@@ -0,0 +1,15 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+// console.log(null >0);
+// console.log(null == 0);
+// console.log(null >= 0); 
+//because equality comparison converts null to a number treating it as 0
+// thats why null>=0 is true and null>0 is false
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// === checks strictly even its datatype
+console.log("2" == 2); // true
+console.log("2" === 2); // false

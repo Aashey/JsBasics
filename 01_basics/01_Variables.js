3089804a5e92bc76d8482a39707c228e8d994246
Aashey <dazewaker12345@gmail.com>
2024-05-25 10:51:44 +0545

    Basics of Javascript

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,13 @@
+const x =1
+let y = "Aashish"
+var z = true
+w = 'James' //not good habit
+let v;
+
+// x = 2 // not alllowed
+// {} = block scope
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([x,y,z,w,v]);
---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// console.log(3 +
+//     3
+// ); // code readability must be high, so this is bad habit
+
+
+let name = "Aashey"
+let age = 26
+let isLoggedIn = false
+
+// primitive data types
+//---------------------------------------
+// number => 2 ^ 53
+// bigint => or large datas
+// string => "" or '' but prefer ""
+// boolean => true or false
+// null => standalone value
+// undefined => not assigned
+// symbol => unique
+
+//object
+// console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+
+
+
